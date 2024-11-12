d6532448b6e67ea6d2400085b7724afec69f837d
Thành Duy <21520774@gm.uit.edu.vn>
2024-11-12 10:44:06 +0700

    2070. Most Beautiful Item for Each Query

---
+++ Most Beautiful Item for Each Query/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Most_Beautiful_Item_for_Each_Query
+{
+    internal class Program
+    {
+        public static int[] MaximumBeauty(int[][] items, int[] queries)
+        {
+            var result = new int[queries.Length];
+            items = items.OrderBy(x => x[0]).ToArray();
+
+            var max = 0;
+
+            for (var i = 0; i < items.Length; i++)
+            {
+                max = Math.Max(max, items[i][1]);
+                items[i][1] = max;
+            }
+
+            for (var i = 0; i < queries.Length; i++)
+            {
+                result[i] = FindMax(items, queries[i]);
+            }
+
+            return result;
+        }
+
+        public static int FindMax(int[][] arr, int n)
+        {
+            var l = 0;
+            var r = arr.Length - 1;
+            var max = 0;
+            while (l <= r)
+            {
+                var m = (r + l) / 2;
+                if (arr[m][0] > n)
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    max = Math.Max(max, arr[m][1]);
+                    l = m + 1;
+                }
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            var result = MaximumBeauty([[1, 2], [3, 2], [2, 4], [5, 6], [3, 5]], [1, 2, 3, 4, 5, 6]);
+            for (var i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
--- Separate Black and White Balls/obj/Debug/net8.0/Separate Black and White Balls.AssemblyInfo.cs
+++ Separate Black and White Balls/obj/Debug/net8.0/Separate Black and White Balls.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Separate Black and White Balls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d13c10e298adfcbf430e7a8abaabc6f325402d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bfd9e459a5a15d80f45ec9c5b5b3e9d79d0793b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Separate Black and White Balls")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Separate Black and White Balls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
